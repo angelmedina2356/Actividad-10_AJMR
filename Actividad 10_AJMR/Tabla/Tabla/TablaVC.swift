6eaa35d611a2b9c5ed8f997e523aa40ecbcd9ae0
angelmedina2356 <58187924+angelmedina2356@users.noreply.github.com>
2021-05-03 17:41:16 -0500

    Add files via upload

---
+++ Actividad 10_AJMR/Tabla/Tabla/TablaVC.swift
@@ -0,0 +1,90 @@
+//
+//  TablaVC.swift
+//  Tabla
+//
+//  Created by L01451229 on 4/28/16.
+//  Copyright © 2016 itesm.mx. All rights reserved.
+//
+
+import UIKit
+//DESARROLLO DE LA ACTIVIDAD SEGUN CANVAS//  //ANGEL JOSUE MEDINA RIVAS - 2795303//
+class TablaVCTableViewController: UITableViewController {
+    let nombres = ["Carlos","Luis","Pedro","Jose"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return nombres.count
+    }
+
+   
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celda", for: indexPath)
+
+        // Configurar celda
+
+        cell.textLabel?.text = nombres[indexPath.row]
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func performSegue(withIdentifier identifier: String , sender: Any?) {
+        
+        if identifier.self == "verDetalle"{
+            let _:ViewController = identifier.description as! ViewController;()
+        }
+            }
+    
+
+}
